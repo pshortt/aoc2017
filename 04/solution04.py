72fd414ed74bacc22c1fec863e384949d0e9a12b
Patrick <pshortt21@gmail.com>
2017-12-16 22:50:29 -0500

    completed 04

--- 04/solution04.py
+++ 04/solution04.py
@@ -1,13 +1,29 @@
 import itertools
 
-def check_passphrases(fname):
-    f = open(fname)
-    for passphrase in f:
-        
-        
+def check_no_dupes(fname):
+    return check_passphrases(fname, make_equality_test())
 
-def check_passphrase(passphrase):
+def check_no_anagrams(fname):
+    return check_passphrases(fname, make_is_anagram())
+    
+def check_passphrases(fname, check):
+    f = open(fname)
+    nvalid = 0
+    for passphrase in f:
+        if check_passphrase(passphrase, check):
+            nvalid += 1
+    f.close()
+    
+    return nvalid
+
+def check_passphrase(passphrase, check):
     combs = itertools.combinations([word for word in passphrase.split()], 2)
     for t in combs:
-        if t[0] == t[1]: return False
+        if check(t[0], t[1]): return False
     return True
+    
+def make_equality_test():
+    return lambda x, y: x == y
+    
+def make_is_anagram():
+    return lambda s, o: sorted([c for c in s]) == sorted([c for c in o])
--- 04/solution04_test.py
+++ 04/solution04_test.py
@@ -7,16 +7,31 @@ class Solution04Test(unittest.TestCase):
         self.foo = 'bar'
         
     def testExample01(self):
-        self.assertEqual(solution04.check_passphrase('aa bb cc dd ee'), True)
+        self.assertEqual(
+            solution04.check_passphrase('aa bb cc dd ee', lambda x, y: x == y),
+                                        True)
         
     def testExample02(self):
-        self.assertEqual(solution04.check_passphrase('aa bb cc dd aa'), False)
+        self.assertEqual(
+            solution04.check_passphrase('aa bb cc dd aa', lambda x, y: x == y),
+                                        False)
         
     def testExample03(self):
-        self.assertEqual(solution04.check_passphrase('aa bb cc dd aaa'), True) 
+        self.assertEqual(
+            solution04.check_passphrase('aa bb cc dd aaa',lambda x, y: x == y),
+                                        True) 
         
-    # def testExample04(self):
-        # self.assertEqual(solution04.check_passphrase(''), '')
+    def testPuzzle01(self):
+        self.assertEqual(solution04.check_no_dupes('puzzle_input.txt'), 386)
+    
+    def testExample05(self):
+        self.assertEqual(solution04.check_no_dupes('example_input1.txt'), 2)
+        
+    def testPuzzle02(self):
+        self.assertEqual(solution04.check_no_anagrams('puzzle_input.txt'), 208)
+    
+    def testExample06(self):
+        self.assertEqual(solution04.check_no_anagrams('example_input2.txt'), 3)
         
 if __name__ == '__main__':
     unittest.main()
