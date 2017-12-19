b71a030e99f2bf720ff15ebcaf805b5818c42bed
pshortt <pshortt21@gmail.com>
2017-12-18 22:44:29 -0500

    refactor 06 tests

--- 06/solution06_test.py
+++ 06/solution06_test.py
@@ -1,25 +1,26 @@
 import unittest
 import solution06
 
-class Solution06Test(unittest.TestCase):        
+class Solution06Test(unittest.TestCase):   
+    @classmethod
+    def setUpClass(self):
+        self.example = solution06.RedistInfLoopCheck([0,2,7,0])
+        self.puzzle = solution06.RedistInfLoopCheck([11,11,13,7,0,15,5,5,4,4,1,1,7,1,15,11])
+     
     def testExample01(self):
-        result = solution06.RedistInfLoopCheck([0,2,7,0])
-        self.assertEqual(result.ctr, 5)
+        self.assertEqual(self.example.ctr, 5)
         
     def testExample02(self):
-        result = solution06.RedistInfLoopCheck([0,2,7,0])
-        self.assertEqual(result.ncycles, 4)
+        self.assertEqual(self.example.ncycles, 4)
         
     def testIterateBanks(self):
         self.assertEqual(solution06.iterate_banks([0,2,7,0]), [2,4,1,2])
         
     def testPuzzle01(self):
-        result = solution06.RedistInfLoopCheck([11,11,13,7,0,15,5,5,4,4,1,1,7,1,15,11])
-        self.assertEqual(result.ctr, 4074)
+        self.assertEqual(self.puzzle.ctr, 4074)
         
-    def testPuzzle01(self):
-        result = solution06.RedistInfLoopCheck([11,11,13,7,0,15,5,5,4,4,1,1,7,1,15,11])
-        self.assertEqual(result.ncycles, 2793)
+    def testPuzzle02(self):
+        self.assertEqual(self.puzzle.ncycles, 2793)
 
 if __name__ == '__main__':
     unittest.main()
