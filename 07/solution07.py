b7566f06e7c27eb8ac5db614875c5412770d7a37
pshortt <pshortt21@gmail.com>
2017-12-21 16:37:11 -0500

    completed 07

--- 07/solution07.py
+++ 07/solution07.py
@@ -21,6 +21,19 @@ class Program():
             weight += p.total_weight()
         return weight
     
+    def balance_report(self):
+        unique_weights = []
+        corr_programs = []
+        for p in self.children:
+            tw = p.total_weight()
+            if tw in unique_weights:
+                corr_programs[unique_weights.index(tw)].append(p.name)
+            else:
+                unique_weights.append(tw)
+                corr_programs.append([p.name])
+        
+        return {'uw':unique_weights, 'cp':corr_programs}
+    
     def __str__(self):
         return '{} ({}) -> {}'.format(self.name,
                                       self.weight,
@@ -55,5 +68,27 @@ class Tower():
             if p.name == pname: return p
         return None
     
+    def balance_tower(self):
+        new_weight = -1
+        delta = 0
+        p = self.root
+        while new_weight < 0:
+            br = p.balance_report()
+            if len(br['uw']) == 2:
+                if len(br['cp'][0]) == 1:
+                    delta = br['uw'][1] - br['uw'][0]
+                    p = self.get_program(br['cp'][0][0])
+                else:
+                    delta = br['uw'][0] - br['uw'][1]
+                    p = self.get_program(br['cp'][1][0])
+            elif len(br['uw']) == 1:
+                new_weight = p.weight + delta
+        return new_weight
+    
     def __str__(self):
         return 'root: {} programs: {}'.format(self.root, self.programs)
+
+if __name__ == '__main__':
+    t = Tower('example_input.txt')
+    print(t.balance_tower())
+    
--- 07/solution07_test.py
+++ 07/solution07_test.py
@@ -7,13 +7,22 @@ class Solution07Test(unittest.TestCase):
         self.example = solution07.Tower('example_input.txt')
         self.puzzle = solution07.Tower('puzzle_input.txt')
     
-    def testExample(self):
+    def testExample01(self):
         self.assertEqual(self.example.root.name, 'tknk')
+
+        
+    def testPuzzle01(self):
+        self.assertEqual(self.puzzle.root.name, 'qibuqqg')
 
     def testTotalWeight(self):
         self.assertEqual(self.example.get_program('padx').total_weight(), 243)
-    def testPuzzle(self):
-        self.assertEqual(self.puzzle.root.name, 'qibuqqg')
+        
+    def testExample02(self):
+        self.assertEqual(self.example.balance_tower(), 60)
+ 
+    def testPuzzle02(self):
+        self.assertEqual(self.puzzle.balance_tower(), 1079)
 
+        
 if __name__ == '__main__':
     unittest.main()
