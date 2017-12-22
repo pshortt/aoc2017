f5ddc12474b9969c327b5cae9ba65b704c0bbd8c
Patrick <pshortt21@gmail.com>
2017-12-22 10:42:17 -0500

    groundwork for 08

---
+++ 08/solution08_test.py
@@ -0,0 +1,12 @@
+import unittest
+import solution08
+
+class Solution08Test(unittest.TestCase):
+    def testExample(self):
+        self.assertTrue(True)
+
+    def testPuzzle(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
