046eb49519ee5d8140a8293f6229fb04168e148d
pshortt <pshortt21@gmail.com>
2018-01-03 16:46:11 -0500

    progress on 10

---
+++ 10/solution10_test.py
@@ -0,0 +1,12 @@
+import unittest
+from solution10 import tie_knot
+
+class Solution10Test(unittest.TestCase):
+    def testExample(self):
+        self.assertEqual(tie_knot([3, 4, 1, 5], 5), 12)
+
+    def testPuzzle(self):
+        self.assertEqual(tie_knot([97,167,54,178,2,11,209,174,119,248,254,0,255,1,64,190]), -1)
+
+if __name__ == '__main__':
+    unittest.main()
