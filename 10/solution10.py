ffd4e6023d6c88def8374724b08c15c40e7482a2
pshortt <pshortt21@gmail.com>
2018-01-04 11:48:07 -0500

    completed 10p1

--- 10/solution10.py
+++ 10/solution10.py
@@ -4,38 +4,18 @@ def tie_knot(lengths, r=256):
     skip = 0
     for length in lengths:
         start = currpos
-        end = (currpos + length) % len(circle)
-        # print("--------------")
-        # print("Circle", circle)
-        # print("Current Position:", currpos, "Skip size:", skip)
-        # print("Start:", start, "End:", end)
-        circle_hist = circle[:]
+        end = ((currpos + length) % len(circle))
+
         if end > start:
-            circle[start:end] = [x for x in reversed(circle[start:end])]
-        elif end == start:
+            rev = [x for x in reversed(circle[start:end])]
+            circle[start:end] = rev
+        elif end != start:
             rev = [x for x in reversed(circle[start:] + circle[:end])]
-            circle[start:] = rev[:-end]
-            circle[:end] = rev[-end:]
-        else:
-            rev = [x for x in reversed(circle[start:] + circle[:end])]
-            circle[start:] = rev[:-end]
-            print(len(circle))
-            circle[:end] = rev[-end:]
-            print(len(circle))
+            pvt = len(rev) - end
+            circle[start:] = rev[:pvt]
+            circle[:end] = rev[pvt:]
         
-        if len(circle) != len(circle_hist):
-            print("Error! circle has been degraded.")
-            print("Old Circle:", circle_hist, "Length: ", len(circle_hist))
-            print("New Circle:", circle, "Length: ", len(circle))
-            print("Start:", start, "End:", end)
-            print("Rev:", rev)
-            return None
-        
-        currpos = (currpos + length + skip) % len(circle)
+        currpos = (currpos + length + skip)  % len(circle)
         skip += 1
-        
-        
-            
-            
-        
+       
     return circle[0]*circle[1]
--- 10/solution10_test.py
+++ 10/solution10_test.py
@@ -6,7 +6,7 @@ class Solution10Test(unittest.TestCase):
         self.assertEqual(tie_knot([3, 4, 1, 5], 5), 12)
 
     def testPuzzle(self):
-        self.assertEqual(tie_knot([97,167,54,178,2,11,209,174,119,248,254,0,255,1,64,190]), -1)
+        self.assertEqual(tie_knot([97,167,54,178,2,11,209,174,119,248,254,0,255,1,64,190]), 8536)
 
 if __name__ == '__main__':
     unittest.main()
