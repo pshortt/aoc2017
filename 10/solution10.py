fa6c704626e31009fff611af4b30b69013fcc2f2
pshortt <pshortt21@gmail.com>
2018-01-04 15:50:07 -0500

    completed 10 part 2

--- 10/solution10.py
+++ 10/solution10.py
@@ -1,21 +1,53 @@
-def tie_knot(lengths, r=256):
-    circle = [x for x in range(0, r)]
-    currpos = 0
-    skip = 0
-    for length in lengths:
-        start = currpos
-        end = ((currpos + length) % len(circle))
-
-        if end > start:
-            rev = [x for x in reversed(circle[start:end])]
-            circle[start:end] = rev
-        elif end != start:
-            rev = [x for x in reversed(circle[start:] + circle[:end])]
-            pvt = len(rev) - end
-            circle[start:] = rev[:pvt]
-            circle[:end] = rev[pvt:]
+class KnotHash():
+    def __init__(self, input, is_part2, r=256):
+        # TODO: implement
+        if isinstance(input, str):
+            self.lengths = [ord(c) for c in input]
+        else:
+            self.lengths = input
         
-        currpos = (currpos + length + skip)  % len(circle)
-        skip += 1
-       
-    return circle[0]*circle[1]
+        self.currpos = 0
+        self.skip = 0   
+        self.sparse = [x for x in range(0, r)]
+        if is_part2:
+            self.lengths.extend([17, 31, 73, 47, 23])
+            for i in range(0, 64):
+                self.tie_knot()
+        else:  
+            self.tie_knot(r)
+        
+        self.reduce()
+        
+    def __str__(self):
+        # TODO: implement
+        return ''.join(["{0:0{1}x}".format(x,2) for x in self.dense])
+    
+    def tie_knot(self, r=256):
+        for length in self.lengths:
+            start = self.currpos
+            end = ((self.currpos + length) % len(self.sparse))
+
+            if end > start:
+                rev = [x for x in reversed(self.sparse[start:end])]
+                self.sparse[start:end] = rev
+            elif end != start:
+                rev = [x for x in reversed(self.sparse[start:] + self.sparse[:end])]
+                pvt = len(rev) - end
+                self.sparse[start:] = rev[:pvt]
+                self.sparse[:end] = rev[pvt:]
+            
+            self.currpos = (self.currpos + length + self.skip)  % len(self.sparse)
+            self.skip += 1
+    
+    def reduce(self, interval=16):
+        reg = 0
+        self.dense = []
+        for i in range(0, len(self.sparse) // interval):
+            slice = self.sparse[reg:reg+interval]
+            # print("Reg: ", reg, "Reg + interval: ", reg+interval)
+            d_ele = slice[0]
+            for s_ele in slice[1:]:
+                d_ele ^= s_ele
+            self.dense.append(d_ele)
+            reg += interval
+    
--- 10/solution10_test.py
+++ 10/solution10_test.py
@@ -1,12 +1,50 @@
 import unittest
-from solution10 import tie_knot
+from solution10 import KnotHash
 
 class Solution10Test(unittest.TestCase):
-    def testExample(self):
-        self.assertEqual(tie_knot([3, 4, 1, 5], 5), 12)
+    @classmethod
+    def setUpClass(self):
+        self.example1 = KnotHash([3, 4, 1, 5], False, 5)
+        self.example2 = KnotHash('\x03\x04\x01\x05', False, 5)
+        self.example3 = KnotHash('', True)
+        self.example4 = KnotHash('AoC 2017', True)
+        self.example5 = KnotHash('1,2,3', True)
+        self.example6 = KnotHash('1,2,4', True)
+        
+        self.puzzle1 = KnotHash([97,167,54,178,2,11,209,174,119,248,254,0,255,1,64,190], False)
+        self.puzzle2 = KnotHash('a\xa76²\x02\x0bÑ\xaew\xf8\xfe\x00ÿ\x01@\xbe', False)
+        self.puzzle3 = KnotHash('97,167,54,178,2,11,209,174,119,248,254,0,255,1,64,190', True)
+        
+    def testExample01(self):
+        res = self.example1.sparse
+        self.assertEqual(res[0]*res[1], 12)
 
-    def testPuzzle(self):
-        self.assertEqual(tie_knot([97,167,54,178,2,11,209,174,119,248,254,0,255,1,64,190]), 8536)
-
+    def testExample02(self):
+        res = self.example2.sparse
+        self.assertEqual(res[0]*res[1], 12)
+        
+    def testExample03(self):
+        self.assertEqual(str(self.example3), 'a2582a3a0e66e6e86e3812dcb672a272')
+    
+    def testExample04(self):
+        self.assertEqual(str(self.example4), '33efeb34ea91902bb2f59c9920caa6cd')
+    
+    def testExample05(self):
+        self.assertEqual(str(self.example5), '3efbe78a8d82f29979031a4aa0b16a9d')
+        
+    def testExample05(self):
+        self.assertEqual(str(self.example6), '63960835bcdc130f0b66d7ff4f6a5a8e')
+        
+    def testPuzzle01(self):
+        res = self.puzzle1.sparse
+        self.assertEqual(res[0]*res[1], 8536)
+        
+    def testPuzzle02(self):
+        res = self.puzzle2.sparse
+        self.assertEqual(res[0]*res[1], 8536)
+        
+    def testPuzzle03(self):
+        self.assertEqual(str(self.puzzle3), '')       
+    
 if __name__ == '__main__':
     unittest.main()
