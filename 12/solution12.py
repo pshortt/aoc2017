0e30748193b821b60e13dd54662d9f8b414827f8
pshortt <pshortt21@gmail.com>
2018-01-08 16:54:34 -0500

    additional progress on 12

--- 12/solution12.py
+++ 12/solution12.py
@@ -3,7 +3,11 @@ class Program():
         words = inp_str.split()
         self.id = int(words[0])
         self.pipes = [int(p.split(',')[0]) for p in words[2:]]
-        
+    
+    def get_nodes(self):
+        res = [id]
+        return res.extend(self.pipes)
+    
     def __str__(self):
         return '{} <-> {}'.format(self.id, self.pipes)
         
@@ -15,3 +19,10 @@ class Village():
     
     def __str__(self):
         return '{}'.format([str(p) for p in self.programs])
+        
+    def get_network(self, id):
+        found_new = True
+        network = []
+        while found_new:
+            
+        
