13c802ee6966443361780c77c83512d7c707d93e
Patrick <pshortt21@gmail.com>
2018-01-08 23:59:57 -0500

    completed 12

--- 12/solution12.py
+++ 12/solution12.py
@@ -23,6 +23,38 @@ class Village():
     def get_network(self, id):
         found_new = True
         network = []
+        current_p = self.get_program(id)
+        path = []
+        traveled = [current_p.id] 
         while found_new:
+            for p in current_p.pipes:   
+                if p not in network:
+                    network.append(p)
+                if p not in traveled:
+                    path.append(p)
+            traveled.append(current_p.id)
+            if len(path) == 0:
+                found_new = False
+            else:
+                current_p = self.get_program(path.pop())
+        network.sort()
+        return network
+    
+    def n_groups(self):
+        ids = [p.id for p in self.programs]
+        found_ids = []
+        n_groups = 0
+        for id in ids:
+            if id not in found_ids:
+                found_ids.extend(self.get_network(id))
+                n_groups += 1
             
+        
+        return n_groups
+            
+    def get_program(self, id):
+        for p in self.programs:
+            if p.id == id:
+                return p
+        return None
         
--- 12/solution12_test.py
+++ 12/solution12_test.py
@@ -3,12 +3,27 @@ from solution12 import Village
 from solution12 import Program
 
 class Solution12Test(unittest.TestCase):
-    def testExample(self):
-        self.assertTrue(True)
+    @classmethod
+    def setUpClass(self):
+        self.example = Village('example_input.txt')
+        self.puzzle = Village('puzzle_input.txt')
+    
+    def testExample01(self):
+        self.assertEqual(self.example.get_network(0), [0, 2, 3, 4, 5, 6])
+        
+    def testExample02(self):
+        self.assertEqual(self.example.get_network(1), [1])
 
-    def testPuzzle(self):
-        self.assertTrue(True)
+    def testPuzzle01(self):
+        self.assertEqual(len(self.puzzle.get_network(0)), 145)
+        
+    def testExample03(self):
+        self.assertEqual(self.example.n_groups(), 2)
+
+    def testPuzzle02(self):
+        self.assertEqual(self.puzzle.n_groups(), 207)
 
 if __name__ == '__main__':
-    # unittest.main()
-    print(Village('example_input.txt'))
+    unittest.main()
+    # v = Village('example_input.txt')
+    # print(v.n_groups())
