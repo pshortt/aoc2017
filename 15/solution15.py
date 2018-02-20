8b81bd1c87004e9664978fa851e26bdb36e57782
pshortt <pshortt21@gmail.com>
2018-02-20 13:15:01 -0500

    completed 15s tests

--- 15/solution15.py
+++ 15/solution15.py
@@ -1,21 +1,19 @@
-def genseq(seed, factor, length=4*10**7, fmult=1, mod=2147483647):
-    seq = []
-    seq.append(seed*factor % mod)
-    while len(seq) < length:
-        n = seq[-1:][0]*factor % mod
-        if n % fmult == 0:
-            seq.append(n)
-    return seq
+def gen(seed, factor, fmult, mod=2147483647):
+    x = seed
+    while True:
+        x *= factor
+        x %= mod
+        if x % fmult == 0:
+            yield x
     
-def judge(a, b, f=16):
-    return binfill(a, f)[-f:] == binfill(b, f)[-f:]
+def count_matches(length, seedA, seedB, fmultA=1, fmultB=1):
+    genA = gen(seedA, 16807, fmultA)
+    genB = gen(seedB, 48271, fmultB)
     
-def binfill(x, f):
-    return bin(x)[2:].zfill(f)
+    ctr = 0
     
-def count_judge(seedA, seedB, length=4*10**7, fmultA=1, fmultB=1, factA=16807, factB=48271):
-    seqA = genseq(seedA, factA, length, fmultA)
-    seqB = genseq(seedB, factB, length, fmultB)
-    to_judge = [(seqA[i], seqB[i]) for i in range(len(seqA))]
-    judged = map(lambda t: judge(*t), to_judge)
-    return sum(judged)
+    for x in range(length):
+        if (next(genA) & 0xFFFF) == (next(genB) & 0xFFFF):
+            ctr += 1
+            
+    return ctr
--- 15/solution15_test.py
+++ 15/solution15_test.py
@@ -1,40 +1,20 @@
 import unittest
-from solution15 import genseq, judge, count_judge
-from solution15_new import gen, count_matches
+from solution15 import gen, count_matches
 
 class Solution15Test(unittest.TestCase):
-    # def testExample01(self):
-        # self.assertEqual(count_judge(65, 8921), 588)
+    def testExample01(self):
+        self.assertEqual(count_matches(4*10**7, 65, 8921), 588)
         
-    # def testPuzzle01(self):
-        # self.assertEqual(count_judge(516, 190), 597)
+    def testPuzzle01(self):
+        self.assertEqual(count_matches(4*10**7, 516, 190), 597)
         
     def testExample02(self):
-        self.assertEqual(count_judge(65, 8921, 5*10**6, 4, 8), 309)
+        self.assertEqual(count_matches(5*10**6, 65, 8921, 4, 8), 309)
         
     def testPuzzle02(self):
-        self.assertEqual(count_judge(516, 190, 5*10**6, 4, 8), -1)
-    
-    def testGenseq01(self):
-        self.assertEqual(genseq(65, 16807, 5), [1092455, 1181022009, 245556042, 1744312007, 1352636452])
-        
-    def testGenseq02(self):
-        self.assertEqual(genseq(8921, 48271, 5), [430625591, 1233683848, 1431495498, 137874439, 285222916])
-     
-    def testJudge01(self):
-        self.assertTrue(judge(245556042, 1431495498))
-        
-    def testJudge02(self):
-        self.assertFalse(judge(1092455, 430625591))
- 
-def genInt():
-    x = -1
-    while True:
-        x += 1
-        yield x
-        
+        self.assertEqual(count_matches(5*10**6, 516, 190, 4, 8), 303)
+
 if __name__ == '__main__':
-    # unittest.main()
-    # print(count_matches(4*10**7))   
-    print(count_matches(5*10**6, 4, 8))
+    unittest.main()
+
     
