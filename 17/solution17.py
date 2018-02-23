e2b811a29d700e1a25e310e31caf41b8f161c810
pshortt <pshortt21@gmail.com>
2018-02-23 14:46:58 -0500

    completed 17

--- 17/solution17.py
+++ 17/solution17.py
@@ -1,14 +1,25 @@
+from collections import deque
+
 def spinlock(steps, n):
-    buffer = [0]
-    pos = 0
+    buffer = deque([0])
     for x in range(n):
-        pos += steps
-        pos %= len(buffer)
-        pos += 1
-        buffer.insert(pos, x + 1)
+        buffer.rotate(-steps)
+        buffer.append(x + 1)
     return buffer
     
 def short_circuit(steps, n=2017):
      buffer = spinlock(steps, n)
-     pos = buffer.index(n)
-     return buffer[(pos + 1) % len(buffer)]
+     # print(buffer)
+     return buffer[0]
+     
+def after_zero(steps, n=50000000):
+    result = -1
+    pos = 0
+    for x in range(n):
+        pos += steps
+        pos %= x + 1
+        pos += 1
+        if pos == 1:
+            result = x + 1
+    return result
+            
--- 17/solution17_test.py
+++ 17/solution17_test.py
@@ -1,20 +1,18 @@
 import unittest
-from solution17 import short_circuit, spinlock
+from solution17 import short_circuit, after_zero, spinlock
 
 class Solution17Test(unittest.TestCase):
-    def testExample(self):
+    def testExample01(self):
         self.assertEqual(short_circuit(3, 4), 3)
         
-    def testPuzzle(self):
+    def testPuzzle01(self):
         self.assertEqual(short_circuit(324), 1306)
         
-    def testPuzzle(self):
-        self.assertEqual(short_circuit(324, 1000000), 1306)
+    def testPuzzle02(self):
+        self.assertEqual(after_zero(324), -1)
         
 if __name__ == '__main__':
-    # unittest.main()
-    for x in range(90):
-        buffer = spinlock(324, x+5)
-        print(*(buffer))
-
-        6,30,19
+    unittest.main()
+    # for x in range(90):
+        # buffer = spinlock(324, x+5)
+        # print(*(buffer))
